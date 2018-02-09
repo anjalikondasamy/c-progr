978e90f8190fc6e04876e8ff212207c6dacc6738
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-02-09 15:56:50 +0530

    c pgm
    good

---
+++ multiple.c
@@ -0,0 +1,16 @@
+3include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+
+printf("\n enter the first five numbers");
+scanf("%d",&first fivenumbers);
+
+for(i=n;i<=5;i++)
+{
+printf("\n multiply of first fivenumbers");
+}
+else{
+retun0;
+}
